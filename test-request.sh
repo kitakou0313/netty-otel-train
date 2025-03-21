283743dd6627cae060c425d6888afe7d93dadd7f
kitakou0313 <kitakou0313@gmail.com>
2025-03-21 22:38:58 +0900

    add: add test script

---
+++ test-request.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# 送信回数
+COUNT=5
+# リクエスト先URL
+URL="http://localhost:8080"
+
+# リクエストを繰り返し送信
+for ((i=1; i<=COUNT; i++))
+do
+  echo "Sending request $i..."
+  curl -X GET "$URL"
+done
+
+echo "All requests sent."
